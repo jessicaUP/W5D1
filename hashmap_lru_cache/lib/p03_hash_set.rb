e28dac2f251bda3e7b0268aef0a5b8b890c688c6
jessicaUP <uphoff.jessica@gmail.com>
2021-03-22 16:41:20 -0400

    Created HashSet

--- hashmap_lru_cache/lib/p03_hash_set.rb
+++ hashmap_lru_cache/lib/p03_hash_set.rb
@@ -7,18 +7,35 @@ class HashSet
   end
 
   def insert(key)
+    num = key.hash
+    unless self.include?(key)
+      self[num] << key
+      @count += 1
+      resize! if @count == num_buckets
+    end
   end
 
   def include?(key)
+    num = key.hash
+    self[num].each { |ele| return true if ele == key }
+    false
   end
 
   def remove(key)
+    num = key.hash
+    if self.include?(key)
+      i = self[num].index(key)
+      self[num].delete_at(i)
+      @count -= 1
+    end
   end
 
   private
 
-  def [](num)
+  def [](num)  #num => the hash code
     # optional but useful; return the bucket corresponding to `num`
+    bucket_idx = num % num_buckets
+    @store[bucket_idx]
   end
 
   def num_buckets
@@ -26,5 +43,9 @@ class HashSet
   end
 
   def resize!
+    keys = @store.flatten
+    @store = Array.new(num_buckets*2) { Array.new }
+    @count = 0
+    keys.each { |key| self.insert(key) }
   end
 end
